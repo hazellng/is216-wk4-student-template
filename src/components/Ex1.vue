ab7536e1ff6d2a9b649a3d98f621577a8e82f428
hazellng <hzlnguyen03@gmail.com>
2026-09-07 08:42:09 +0800

    change1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,15 +3,16 @@
 
     // Add code here
     // challenge: use array  
-    
+    const message = ref("Choose your favorite fruit:  ")
+    const fruits = ref(['apple', 'orange', 'peach'])
 </script>
 
 <template>
     {{ message }} 
    
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[0] }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[1] }} </label> 
+    <label for=""><input type="radio" name="fruit"> {{ fruits[2] }} </label> 
     
 </template>
 
